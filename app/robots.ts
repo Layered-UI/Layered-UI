c9e9cae0d30400b561b510ca0527f8bd24c89015
Tristan Hendricks <moonknight1355@gmail.com>
2026-05-15 23:46:52 +0200

    feat: add robots.txt configuration for search engine indexing

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/'],
     },
-    sitemap: 'https://layeredui.vercel.app/sitemap.xml',
+    sitemap: 'https://layered-ui.vercel.app/sitemap.xml',
   }
 }
